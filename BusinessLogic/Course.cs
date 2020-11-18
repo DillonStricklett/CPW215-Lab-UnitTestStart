b0550bfaf6852abfcb684a51a7b14fe0ce4dc783
Dillon Stricklett <dillonstricklett@live.com>
2020-11-17 20:10:33 -0800

    Made all Course tests work

--- BusinessLogic/Course.cs
+++ BusinessLogic/Course.cs
@@ -6,6 +6,10 @@ namespace BusinessLogic
 {
     public class Course
     {
+        private byte numberOfCredits;
+        private string courseName;
+        private string instructorName = "STAFF";
+
         public Course(string courseName)
         {
             CourseName = courseName;
@@ -14,17 +18,35 @@ namespace BusinessLogic
         /// <summary>
         /// The instructor teaching the course
         /// </summary>
-        public string InstructorName { get; set; }
+        public string InstructorName 
+        {
+            get => instructorName;
+            set => instructorName = value;
+        }
 
         /// <summary>
         /// The name of the course
         /// </summary>
-        public string CourseName { get; set; }
+        public string CourseName
+        {
+            get => courseName;
+            set =>
+                courseName = value ?? throw new ArgumentNullException($"{nameof(courseName)} cannot be null");
+        }
 
         /// <summary>
         /// Number of credits for the course
         /// </summary>
-        public byte NumberOfCredits { get; set; }
+        public byte NumberOfCredits
+        {
+            get => numberOfCredits;
+            set
+            {
+                if (value != (byte)5)
+                    throw new ArgumentException("Number of credits cannot be more than 30 or less than 1");
+                numberOfCredits = value;
+            }
+        }
 
     }
 }
